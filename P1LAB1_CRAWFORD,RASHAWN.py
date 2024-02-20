c21abbf34454ceeec6fb0950323be893b4790b0d
Rashawn117 <160627886+Rashawn117@users.noreply.github.com>
2024-02-20 11:01:14 -0500

    Add files via upload

---
+++ P1LAB1_CRAWFORD,RASHAWN.py
@@ -0,0 +1,11 @@
+# First Last
+# 2/13/2024
+# P1LAB1
+# Using input and output
+
+#create a variable and get input from user
+name= input("Enter your name: ")
+
+#output a message using the variable
+print("HEY" , name , "\n" + "Welcome to Zy books!")
+#print("Welcome to Zy books!")
