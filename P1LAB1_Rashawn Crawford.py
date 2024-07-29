69dcc85d6582467b9d1b14df981fbb11356cc9ef
Rashawn117 <160627886+Rashawn117@users.noreply.github.com>
2024-07-29 14:46:46 -0400

    Update and rename P1LAB1_CRAWFORD,RASHAWN.py to P1LAB1_Rashawn Crawford.py

---
+++ P1LAB1_Rashawn Crawford.py
@@ -0,0 +1,21 @@
+# Rashawn Crawford
+ # 02-09-2024
+ # P1LAB1
+ # This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+
+
+
+# showing how to print it using input plus multiple variables
+first=input("What is your name?")
+w = "Hey"
+x = "Welcome"
+y = "to"
+z = "zyBooks!"
+print(w, first)
+print(x, y, z)
+
+print('\n')
+
+# showing how to print it with only 1 variable
+print('Hey', first)
+print('Welcome to Zybooks!')
